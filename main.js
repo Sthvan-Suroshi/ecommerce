fad079d6bac74df8af0915f373037d867c2d0cb2
Sthvan-Suroshi <123sthavansuroshi@gmail.com>
2023-11-24 21:48:22 +0530

    made responsive

---
+++ main.js
@@ -0,0 +1,12 @@
+function op(){
+    let a = document.querySelector("#sidebar");
+    a.style.display="block"
+}
+
+function cl(){
+    let b = document.querySelector("#sidebar")
+    {
+        b.style.display="none"
+
+    }
+}
